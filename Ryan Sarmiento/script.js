fe81b1576d3df1d341e040c9112bfc4072f2c4d2
SARMIENTO834 <ryan.sarmiento@evsu.edu.ph>
2025-01-06 12:24:56 +0800

    Add files via upload

---
+++ Ryan Sarmiento/script.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        title: "Project 1",
+        description: "JavaScript Assignment.",
+        image: "bg-img4.jpg"
+    },
+    {
+        title: "Project 2",
+        description: "Bio Data.",
+        image: "bg-img7.jpg"
+    },
+    {
+        title: "Project 3",
+        description: "my journey about creating this project.",
+        image: "bg-img3.jpg"
+    }
+];
+
+
+function showAlert() {
+    alert('Thank you for visiting my portfolio!');
+}
